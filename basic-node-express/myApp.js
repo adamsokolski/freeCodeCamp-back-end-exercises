4657a8cb1254e325f528eb41358f391e19234fba
idKrazu <idkrazu@gmail.com>
2021-11-26 19:23:15 +0100

    ✨ Exercises done

--- basic-node-express/myApp.js
+++ basic-node-express/myApp.js
@@ -1,4 +1,54 @@
 var express = require("express");
 var app = express();
+const bodyParser = require("body-parser");
+
+/*
+app.get("/", function(req, res) {
+  res.send("Hello Express")
+})
+*/
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  const message =
+    process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON" : "Hello json";
+  res.json({ message: message });
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
+});
+
+app
+  .route("/name")
+  .get((req, res) => {
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+  })
+  .post((req, res) => {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+  });
 
 module.exports = app;
