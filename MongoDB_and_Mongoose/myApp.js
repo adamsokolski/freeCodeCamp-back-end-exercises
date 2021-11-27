52ad56f6a89c98d8ad6ee0256e7b5b97a4c78ba3
idKrazu <idkrazu@gmail.com>
2021-11-27 19:19:19 +0100

    ✨ MongoDB ex. 6

--- MongoDB_and_Mongoose/myApp.js
+++ MongoDB_and_Mongoose/myApp.js
@@ -55,7 +55,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, person) => {
+    if (err) return done(err);
+    done(null, person);
+  });
 };
 
 const findPersonById = (personId, done) => {
